91c9cc448ac6c562eb03140fd68b0ea00af44049
MatthewJRBradbury <48898619+MatthewJRBradbury@users.noreply.github.com>
2024-06-11 02:17:52 +1000

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lotto_checker/',
   test: {
     globals: true,
     environment: 'jsdom',
